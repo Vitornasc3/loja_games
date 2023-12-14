6317bb5b76a50ead70d794573e9f4549e932a7be
Vitor Ferreira <vitornasc673@gmail.com>
2023-12-14 14:10:23 -0300

    Implementação do CRUD da classe Produto

---
+++ src/main/java/com/generation/lojagames/controller/ProdutoController.java
@@ -0,0 +1,73 @@
+package com.generation.lojagames.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.generation.lojagames.model.Produto;
+import com.generation.lojagames.repository.ProdutoRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAll() {
+		return ResponseEntity.ok(produtoRepository.findAll());
+
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> getById(@PathVariable Long id) {
+		return produtoRepository.findById(id).map(produto -> ResponseEntity.ok(produto))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Produto>> getByNome(@PathVariable String nome){
+		return ResponseEntity.ok(produtoRepository.findAllByNomeContainingIgnoreCase(nome));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto){
+		return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto){
+		if(produtoRepository.existsById(produto.getId()))
+			return ResponseEntity.status(HttpStatus.OK).body(produtoRepository.save(produto));
+		else
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteById(@PathVariable Long id) {
+		Optional<Produto> checagem = produtoRepository.findById(id);
+		if(checagem.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		produtoRepository.deleteById(id);
+	}
+
+}
---
+++ src/main/java/com/generation/lojagames/model/Produto.java
@@ -0,0 +1,82 @@
+package com.generation.lojagames.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 120)
+	@Size(max = 120, message = "O atributo nome pode conter até 120 caracteres")
+	@NotBlank(message = "O atributo nome deve ser preenchido")
+	private String nome;
+
+	@Column(length = 30)
+	@Size(max = 30, message = "O atributo plataforma pode conter até 30 caracteres")
+	@NotBlank(message = "O atributo plataforma deve ser preenchido")
+	private String plataforma;
+
+	@DecimalMin(value = "0.0", inclusive = false)
+	@Digits(integer = 3, fraction = 2)
+	private BigDecimal valor;
+
+	@Column(nullable=false)
+	@Size(max = 1000, message = "O link da imagem deve conter até 1000 caracteres")
+	private String imagem;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public String getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+		
+}
